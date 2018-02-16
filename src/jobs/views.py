9c75ea02ea6d5d7af23a7b2dba7d04641ff11cca
porschebestcar <porschebestcar@gmail.com>
2018-02-16 09:39:57 +0800

    class based view

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,6 +1,8 @@
 import random
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 # Create your views here.
 # Function based views
 def home(request):
@@ -19,15 +21,19 @@ def home(request):
     }
     return render(request, "home.html", context)
 
-
 def about(request):
     context = {
-
     }
     return render(request, "about.html", context)
 
 def contact(request):
     context = {
-
     }
     return render(request, "contact.html", context)
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "contact.html", context)
+class ContactView(TemplateView):
+    template_name = 'contact.html'
--- src/zerojetlag/urls.py
+++ src/zerojetlag/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from jobs.views import home, about, contact
+from jobs.views import home, about, contact, ContactView, ContactTemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
     url(r'^about/$', about),
-    url(r'^contact/$', contact)
+    url(r'^contact/$', ContactTemplateView.as_view()),
 ]
